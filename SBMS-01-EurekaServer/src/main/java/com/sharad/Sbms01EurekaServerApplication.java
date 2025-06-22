dbe29f6d87e9cf8ed79fc352a144f6c4292e4ec1
skyadav456 <sharadk4545@gmail.com>
2025-06-22 11:45:57 +0530

    initial setup

---
+++ SBMS-01-EurekaServer/src/main/java/com/sharad/Sbms01EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.sharad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Sbms01EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbms01EurekaServerApplication.class, args);
+	}
+
+}
