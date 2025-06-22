8ed0bc4ff01bbb295c0b29185f351733248f8751
skyadav456 <sharadk4545@gmail.com>
2025-06-22 12:59:10 +0530

    initial

---
+++ SBMS-04-Consumer-MS-Communication/src/test/java/com/sharad/Sbms04ConsumerMsCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbms04ConsumerMsCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
